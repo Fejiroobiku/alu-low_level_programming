d93c5a1663fc6bb7ffa76a77595a9ca31b24072c
Fejiroobiku <o.obiku@alustudent.com>
2025-09-22 10:31:55 +0100

    H

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char winners[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(winners[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
